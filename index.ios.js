61b2c5fe9f49ca4da28da06e2c63e0550e7f1fb8
patorndev <di_patorani@msn.com>
2017-06-26 17:05:40 +0700

    Initial Commit # Lesson 41

---
+++ index.ios.js
@@ -0,0 +1,17 @@
+// Code for ios
+
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumsList from './src/components/AlbumsList';
+
+// Create Component
+const App = () => (
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumsList />
+  </View>
+);
+
+// Render to Device
+AppRegistry.registerComponent('albums', () => App);
